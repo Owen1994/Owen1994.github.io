48b6258ddd816d4f10168c917ddd1d2dc2c0abd4
Owen <xupeiwen@live.com>
2018-04-05 14:27:42 +0800

    Owen cinema

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Cheader from './components/header.vue'
+export default {
+  name: 'app',
+  components: {
+    'v-header': Cheader
+  }
+}
+</script>
+
+<style> 
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/content/coming_soon.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="in_theaters">
+        <ul class="ul1 clearfix">
+            <li v-for="item in movieList" v-bind:key="item.id">
+                <a><img :src="item.images.small"></a>
+                <p>{{ item.title }}</p>
+            </li>
+        </ul>
+        <div class="indicator_container">
+            <ul class="pageIndicator">
+                <li class="pre" @click="back()"><</li>
+                <li class="num" v-for="item in pageIndicator" :class="{ indicatorActive: item=='1' }" v-bind:key="item" @click="changePage(item)">{{ item }}</li>
+                <li class="next" @click="forward()">></li>
+            </ul>
+        </div>
+        <i class="active" ref="profile" @click="iClick">123</i>
+    </div> 
+</template>
+<script>
+    export default {
+        name: 'in_theaters',
+        data() {
+            return {
+                movieList:[],
+                totalNum:null,
+                pageTotal:null,
+                pageNum:0,
+                pageIndicator:[]
+            }
+        },
+        methods: {
+            back() {
+                var firstNum = 0;
+                this.pageNum = this.pageNum - 1;
+                this.pageNum = this.pageNum <= firstNum ? firstNum : this.pageNum;
+                this.sendAjax();
+                this.indicatorActive();
+            },
+            forward(e) {
+                var lastNum = this.pageTotal - 1;
+                this.pageNum = this.pageNum + 1;
+                this.pageNum = this.pageNum >= lastNum ? lastNum : this.pageNum;
+                this.sendAjax();
+                this.indicatorActive();
+            },
+            changePage(page){
+                this.pageNum = page - 1;
+                this.sendAjax();
+                this.indicatorActive();
+            },
+            getData(pageNumer) {
+                var _this = this;
+                var pageIndicatorNum = document.querySelectorAll(".pageIndicator .num");
+                this.$http.jsonp('https://api.douban.com/v2/movie/coming_soon?start='+this.pageNum*20, {method: 'GET'}).then(function (response) {
+                    var arr = response.body.subjects;
+                    arr.forEach(function(v) {
+                        _this.movieList.push(v);
+                    })
+                    this.total = response.body.total;
+                    this.pageTotal = Math.ceil(this.total / 20);
+                    for(var i=1;i<this.pageTotal+1;i++) {
+                        this.pageIndicator.push(i);
+                    }
+                });
+            },
+            indicatorActive() {
+                var pageIndicatorNum = document.querySelectorAll(".pageIndicator .num");
+                for(var i=0;i<pageIndicatorNum.length;i++) {
+                    pageIndicatorNum[i].classList.remove("indicatorActive");
+                }
+                pageIndicatorNum[this.pageNum].classList.add("indicatorActive")
+            },
+            sendAjax() {
+                this.$http.jsonp('https://api.douban.com/v2/movie/coming_soon?start='+this.pageNum*20, {
+                    method: 'GET',
+                    before(request) {
+                        if (this.previousRequest) {
+                            this.previousRequest.abort();
+                        }
+                        this.previousRequest = request;
+                    }
+                }).then(function (response) {
+                    var arr = response.body.subjects;
+                    this.movieList = arr;
+                });
+            },
+            iClick() {
+                this.$refs.profile.className = "";
+            }
+        },
+        created() {
+            this.getData(0);
+        },
+        updated() {
+            console.log(this.$refs.profile,this.$root.$data)
+        }
+    }
+</script>
+<style>
+
+</style>
